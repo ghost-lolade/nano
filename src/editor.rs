556e87617e82db05bb6321e7b7f90efd210e58ce
Ghost <wahlolade@gmail.com>
2024-01-08 18:33:33 +0100

    moved clear_screen and flush to terminal.rs

--- src/editor.rs
+++ src/editor.rs
@@ -1,10 +1,12 @@
-use std::io::{self, stdout};
+use crate::Terminal;
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
@@ -12,38 +14,59 @@ impl Editor {
         let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
-            if let Err(error) = self.process_keypress() {
+            if let Err(error) = self.refresh_screen() {
                 die(error);
             }
             if self.should_quit {
                 break;
             }
+
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
         }
 
     }
     pub fn default() -> Self {
-        Self{should_quit: false}
+        Self{
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialized terminal"),
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
+
+        if self.should_quit {
+            println!("Goodbye.\r"); 
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(0, 0);
+        }
+
+        Terminal::flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
+        
         match pressed_key {
-            Key::Ctrl('q') => self.should_quit = true,
+            Key::Ctrl('c') => self.should_quit = true,
             _ => (),
         }
         Ok(())
     }
+
+    fn draw_rows(&self) {
+        for _ in 0..self.terminal.size().height {
+            print!("~\r");
+        }
+    }
     
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
 fn die(e: std::io::Error) {
+    Terminal::clear_screen();
     panic!("{}", e);
 }
